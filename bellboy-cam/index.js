bffe1c2cf95c8d743ebefc37d481fcf7aabcdc3f
erikzaadi <erik.zaadi@gmail.com>
2015-04-27 12:35:40 +0300

    Troubleshooted

--- bellboy-cam/index.js
+++ bellboy-cam/index.js
@@ -20,15 +20,15 @@ var client = s3.createClient({
 
 apiCaller.on('takeSnapshot', function(){
   console.log("takeSnapshot");
-  var now = new Date();
+  var now = moment();
   var expires = now.clone().add(7, "days").toDate();
-  var imageFileName = oment().format("YYYY-MM-DD__hh-mm-ss.png");
+  var imageFileName = now.format("YYYY-MM-DD__hh-mm-ss.png");
   var commandWithTimeStamp = fsWebCamCommand + path.join(expressStaticImagesPath, imageFileName);
   console.log("running", commandWithTimeStamp);
-  exec(fsWebCamCommand, function(error, stdout, stderr){
+  exec(commandWithTimeStamp, function(error, stdout, stderr){
     if (!error){
       console.log("snapshotTaken, saving with s3", imageFileName);
-      var key = (config.prefix ? config.prefix : "") + "/"  + imageFileName;
+      var key = (config.prefix ? config.prefix : "") + imageFileName;
       var params = {
         localFile: path.join(expressStaticImagesPath, imageFileName),
         s3Params: {
@@ -43,9 +43,14 @@ apiCaller.on('takeSnapshot', function(){
         console.error("unable to upload:", err.stack);
       });
       uploader.on('end', function() {
-        console.log("snapshotTaken, saved to s3");
         var imagePublicUrl = s3.getPublicUrlHttp(config.bucket, key);
+        console.log("snapshotTaken, saved to s3", imagePublicUrl);
         apiCaller.emit('snapshotTaken', imagePublicUrl);
+          exec("rm " + params.localFile, function(e, o, se){
+            if (!e){
+              console.log("deleted snapshot", params.localFile);
+            }
+        });
       });
     }
   });
